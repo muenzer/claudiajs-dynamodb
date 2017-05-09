41fc2e72e03b1032ccd6611f651191492e4726d9
Chris Muenzer <chris.muenzer@gmail.com>
2017-05-09 20:51:39 +0200

    added missing lib.js

---
+++ lib.js
@@ -0,0 +1,17 @@
+var exports = module.exports = {};
+
+//configuration
+exports.dynamo = require('./lib/dynamo');
+
+//operations
+exports.createTable = require('./lib/create-table');
+exports.create = require('./lib/create');
+exports.delete = require('./lib/delete');
+exports.get = require('./lib/get');
+exports.query = require('./lib/query');
+exports.scan = require('./lib/scan');
+exports.update = require('./lib/update');
+
+//help functions
+exports.querystring = require('./lib/querystring');
+exports.buildLink = require('./lib/buildlink');
