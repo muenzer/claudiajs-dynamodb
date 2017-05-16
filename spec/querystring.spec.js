98c83bc0115baea312db1e72316e73a5dd43f68f
Chris Muenzer <chris.muenzer@gmail.com>
2017-05-16 21:54:30 +0200

    fixed error in filter

--- spec/querystring.spec.js
+++ spec/querystring.spec.js
@@ -41,6 +41,17 @@ describe('A function for parsing query paramters', function() {
     expect(options.embed.relId).toBe('tableId');
     expect(options.embed.relLabel).toBe('tables');
   });
+  it('returns an options object with filter', function () {
+    queryString = {
+      id: 'abc',
+      color: 'red'
+    };
+    var options = lib.querystring(queryString);
+    expect(options).toBeDefined();
+    expect(options.filter).toBeDefined();
+    expect(options.filter.id).toBe('abc');
+    expect(options.filter.color).toBe('red');
+  });
   it('returns an empty object', function() {
     var options = lib.querystring(queryString);
     expect(typeof(options)).toBe('object');
