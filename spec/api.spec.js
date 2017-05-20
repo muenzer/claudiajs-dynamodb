09a3a3b2205b7e4dee659ff5d0c43cb5745db972
Chris Muenzer <chris.muenzer@gmail.com>
2017-05-20 18:27:07 +0200

    added basic test for api

--- spec/api.spec.js
+++ spec/api.spec.js
@@ -1,76 +1,109 @@
-// var AWS = require('aws-sdk');
-// var lib = require('../index.js');
-// var dynamo = {};
-// var apiHelper = require('../api.js');
-//
-// var dynamoconfig = {
-//   endpoint: 'http://localhost:8000',
-//   region: 'someregion',
-//   accessKeyId: 'test',
-//   secretAccessKey: 'test'
-// };
-//
-// dynamo = new lib.dynamo(dynamoconfig);
-// dynamo.tableName = 'test';
-//
-// var api = apiHelper('test', dynamo);
-//
-//
-// describe('stock api', function () {
-//   beforeEach(function () {
-//
-//   });
-//
-//   it('that creates a table', function (done) {
-//     var params = {
-//       TableName: dynamo.tableName,
-//       AttributeDefinitions: [
-//       { 'AttributeName': 'id', 'AttributeType': 'S' },
-//       ],
-//       KeySchema: [
-//       { 'AttributeName': 'id', 'KeyType': 'HASH' },
-//       ],
-//       ProvisionedThroughput: {
-//         ReadCapacityUnits: 1,
-//         WriteCapacityUnits: 1
-//       },
-//     };
-//
-//     var response = lib.createTable(params, dynamo);
-//
-//     response.then(function (response) {
-//       expect(response).toBeDefined();
-//       done();
-//     });
-//   });
-//
-//   describe('checks api functions', function () {
-//     var lambdaContextSpy;
-//
-//     beforeEach(function () {
-//       lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
-//     });
-//
-//     describe('gets an object', function () {
-//       api.proxyRouter({
-//         requestContext: {
-//           resourcePath: '/foo',
-//           httpMethod: 'GET'
-//         },
-//         queryString: {
-//           id: '1'
-//         }
-//       }, lambdaContextSpy);
-//     });
-//   });
-//
-//   it('deletes the table', function (done) {
-//     var params = {
-//       TableName: dynamo.tableName,
-//     };
-//     dynamo.raw.deleteTable(params, function(err, data) {
-//       expect(err).toBeNull();
-//       done();
-//     });
-//   });
-// });
+var ApiBuilder = require('claudia-api-builder');
+var AWS = require('aws-sdk');
+var lib = require('../index.js');
+var dynamo = {};
+
+var dynamoconfig = {
+  endpoint: 'http://localhost:8000',
+  region: 'someregion',
+  accessKeyId: 'test',
+  secretAccessKey: 'test'
+};
+
+var dynamo = new lib.dynamo(dynamoconfig);
+dynamo.tableName = 'test';
+
+var api = new ApiBuilder();
+api = lib.api('foo', api, dynamo);
+
+
+describe('stock api', function () {
+  beforeAll(function () {
+    var dynamoconfig = {
+      endpoint: 'http://localhost:8000',
+      region: 'someregion',
+      accessKeyId: 'test',
+      secretAccessKey: 'test'
+    };
+
+    var dynamo = new lib.dynamo(dynamoconfig);
+    dynamo.tableName = 'test';
+
+    var api = new ApiBuilder();
+    api = lib.api('foo', api, dynamo);
+  });
+
+  beforeAll(function (done) {
+    var params = {
+      TableName: dynamo.tableName,
+      AttributeDefinitions: [
+      { 'AttributeName': 'id', 'AttributeType': 'S' },
+      ],
+      KeySchema: [
+      { 'AttributeName': 'id', 'KeyType': 'HASH' },
+      ],
+      ProvisionedThroughput: {
+        ReadCapacityUnits: 1,
+        WriteCapacityUnits: 1
+      },
+    };
+
+    var response = lib.createTable(params, dynamo);
+
+    response.then(function (response) {
+      done();
+    });
+  });
+
+  afterAll(function (done) {
+    var params = {
+      TableName: dynamo.tableName,
+    };
+    dynamo.raw.deleteTable(params, function(err, data) {
+      done();
+    });
+  });
+
+  describe('checks api functions', function () {
+    var lambdaContextSpy;
+
+    beforeEach(function () {
+      lambdaContextSpy = jasmine.createSpyObj('lambdaContext', ['done']);
+    });
+
+    it('posts an object', function (done) {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/foo',
+          httpMethod: 'POST'
+        },
+        body: {
+          name: 'foo'
+        }
+      }, lambdaContextSpy)
+      .then(function () {
+        console.log(lambdaContextSpy.done);
+        expect(lambdaContextSpy.done).toHaveBeenCalled();
+        expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({statusCode:200}));
+      })
+      .then(done, done.fail);
+    });
+
+    it('gets an object', function (done) {
+      api.proxyRouter({
+        requestContext: {
+          resourcePath: '/foo',
+          httpMethod: 'GET'
+        },
+        queryString: {
+          id: '1'
+        }
+      }, lambdaContextSpy)
+      .then(function () {
+        expect(lambdaContextSpy.done).toHaveBeenCalled();
+        expect(lambdaContextSpy.done).toHaveBeenCalledWith(null, jasmine.objectContaining({statusCode:200}));
+      })
+      .then(done, done.fail);
+    });
+  });
+});
